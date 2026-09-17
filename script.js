3a36227afc4ca7e149dd60847941d052698479a4
laurenhauck1code <laurenhauck1@gmail.com>
2026-09-17 18:49:20 -0400

    Update script.js
    adding JS

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const bakeryItems = [
+    { id: "signature-loaf", name: "Signature Loaf" },
+    { id: "croissant", name: "Croissant" },
+    { id: "celebration-cake", name: "Celebration Cake" }
+];
 
+const bakeryItems = [
+    { id: "signature-loaf", name: "Signature Loaf" },
+    { id: "croissant", name: "Croissant" },
+    { id: "celebration-cake", name: "Celebration Cake" }
+];
